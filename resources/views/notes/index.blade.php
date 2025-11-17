cae89b6c9ae20f4521a99869d894e743b4276b19
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-17 15:26:59 -0300

    feat: adiciona rota do método show

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -7,12 +7,19 @@
 </head>
 <body>
     <h1>DailyNotes</h1>
-    <h3>Notas</h3>
+
+    <div style="display: flex; justify-content: space-between;">
+        <h3>Notas</h3>
+        <button>
+            <a href="{{ route('notes.create') }}">Nova nota</a>
+        </button>
+    </div>
 
      @foreach($notes as $note)
         <div style="border: 1px solid black; margin-bottom: 10px; padding: 5px;">
             <h3>{{ $note->title }}</h3>
             <p>{{ $note->text }}</p>
+            <a href="{{ route('notes.show', $note) }}">Ver mais</a>
         </div>
     @endforeach
 </body>
