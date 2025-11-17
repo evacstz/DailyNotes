8ceeba596c00dd1055c041ae3c9376c263d0456a
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-17 16:02:27 -0300

    feat: implementa rota do destroy crud de notas

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
 Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
 Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
 Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
 
 
 // BREEZE
