8e415d3a720783da80c79b68def913c638ae8d49
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 11:33:32 -0300

    feat: cria index das checklists

---
+++ resources/views/checklists/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daily Notes</title>
+</head>
+<body>
+    <h1>Daily Notes</h1>
+
+    <div style="display: flex; justify-content: space-between;">
+        <h3>Checklists</h3>
+        <button>
+            <a href="{{ route('checklists.create') }}">Nova lista</a>
+        </button>
+    </div>
+
+    @foreach($checklists as $checklist)
+        <div style="border: 1px solid black; margin-bottom: 10px; padding: 5px;">
+            <h3>{{ $checklist->title }}</h3>
+            <a href="{{ route('checklists.show', $checklist) }}">Ver lista</a>
+        </div>
+    @endforeach
+</body>
+</html>
