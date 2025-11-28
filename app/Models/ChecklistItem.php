7343c7f182b949444e9918552d34f38d9458bbc2
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 10:04:23 -0300

    feat: cria model dos itens da checklist

---
+++ app/Models/ChecklistItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChecklistItem extends Model
+{
+    protected $fillable = ['checklist_id', 'text', 'concluido'];
+
+    public function checklist() {
+        return $this->belongsTo(Checklist::class);
+    }
+}
