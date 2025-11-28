cb2d8dbf215f61b1fa3c9cf01164ceb94d1b5a3d
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 09:49:28 -0300

    feat: implementa rota de checklists

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,20 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // NOTES
-Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
-Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
-Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
-Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
-Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
-Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
-Route::delete('notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
-aaaaa
+Route::resource('/notes', NoteController::class);
+
+// EVENTS
+Route::resource('/events', EventController::class);
+
+// CHECKLIST
+Route::resource('/checklists', ChecklistController::class);
 
 // BREEZE
 Route::get('/dashboard', function () {
