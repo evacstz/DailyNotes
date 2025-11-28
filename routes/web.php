c2faa0b4321c67afa10fefea98f4e3aa5b15918d
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 19:29:44 -0300

    fix: debuga rotas de checklist items

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,9 @@ Route::resource('/events', EventController::class);
 
 // CHECKLIST
 Route::resource('/checklists', ChecklistController::class);
-// CHECKLIST ITEMS
-Route::post('/checklists/{checklist}/items', [ChecklistItemController::class, 'store'])->name('items.store');
-Route::put('/checklists/items/{item}', [ChecklistItemController::class, 'update'])->name('items.update');
-Route::delete('/checklists/items/{item}', [ChecklistItemController::class, 'destroy'])->name('items.destroy');
-Route::patch('/checklists/{checklist}/items/toggle', [ChecklistItemController::class, 'toggle'])->name('items.toggle');
+// CHECKLIST ITEM
+Route::patch('/items/{item}/toggle', [ChecklistItemController::class, 'toggle'])->name('items.toggle');
+Route::delete('/items/{item}', [ChecklistItemController::class, 'destroy'])->name('items.destroy');
 
 // BREEZE
 Route::get('/dashboard', function () {
