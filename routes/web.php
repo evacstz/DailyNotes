99290278c75b76628e21d1b7bcbf34253091b3ea
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 08:46:27 -0300

    feat:simplificando rotas

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show')
 Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
 Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
 Route::delete('notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
-
+aaaaa
 
 // BREEZE
 Route::get('/dashboard', function () {
