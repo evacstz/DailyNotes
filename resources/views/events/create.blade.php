8c08bd010f3cb05b711bce2149c896d73fa4f9d2
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 09:16:49 -0300

    feat: implementa view de criar eventos

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daily Notes</title>
+</head>
+<body>
+    <h1>Criar novo evento</h1>
+
+    <form action="{{ route('events.store') }}" method="post">
+        @csrf
+        <label for="title">Evento:</label>
+        <input type="text" name="title">
+
+        <label for="date">Data:</label>
+        <input type="date" name="date">
+
+        <label for="time">Hora:</label>
+        <input type="time" name="time">
+        
+        <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
