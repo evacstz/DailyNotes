f210d305f77d5b52b57f0a9cdad0c0e2c614d1fa
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 11:35:21 -0300

    feat: cria controller ChecklistItem

--- app/Models/ChecklistItem.php
+++ app/Models/ChecklistItem.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChecklistItem extends Model
 {
+    protected $table = 'checklists_items';
+    
     protected $fillable = ['checklist_id', 'text', 'concluido'];
 
     public function checklist() {
