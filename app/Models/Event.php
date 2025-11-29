e01a5caca43e805ea0b273cd0529e4e1c78dd413
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-11-28 21:33:50 -0300

    chore: remove comentário de método sem utilização

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,11 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = ['title', 'date', 'time'];
-
-    // provisório
-    // protected function date(): Attribute {
-    //     return Attribute::make(
-    //         get: fn ($value) => \Carbon\Carbon::parse($value)->format('d/m/Y'),
-    //     );
-    // }
 }
