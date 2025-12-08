f9fe34ae1c5e4a84727ee12ec4f8abfe61cb7a4b
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-12-07 22:54:34 -0300

    feat: adiciona botão perfil em lembretes

--- resources/views/reminders/index.blade.php
+++ resources/views/reminders/index.blade.php
@@ -29,6 +29,8 @@
                     <a href="{{ route('wishlists.index') }}" class="text-pink-600 hover:text-pink-800 transition-colors">Lista de desejos</a>
                     <a href="{{ route('reminders.index') }}" class="text-pink-700 font-bold ">Lembretes</a>
 
+                    <a href="{{ route('profile.edit') }}" class="text-pink-600 hover:text-pink-800 transition-colors">Perfil</a>
+
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <button type="submit" class="border border-pink-600 rounded-md px-4 py-1 text-pink-600 hover:bg-pink-50 hover:text-pink-800 transition-colors font-bold text-sm">Sair</button>
