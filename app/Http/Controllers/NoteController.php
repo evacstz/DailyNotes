1d0fb9d03f4a71e4237bf93562e0c989da6a7f24
evacstz <eva.cordeiro@aluno.ce.gov.br>
2025-12-07 23:34:18 -0300

    feat:implementa form request no NoteController

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use App\Http\Requests\NoteRequest;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
@@ -27,9 +28,9 @@ class NoteController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(NoteRequest $request)
     {
-        Note::create($request->all());
+        Note::create($request->validated());
         return redirect()->route('notes.index');
     }
 
@@ -52,9 +53,9 @@ class NoteController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Note $note)
+    public function update(NoteRequest $request, Note $note)
     {
-        $note->update($request->all());
+        $note->update($request->validated());
         return redirect()->route('notes.show', $note);
     }
 
